e69ab561bb8505cf38bfb9feaaa13ed3a5e67267
Mike Nicholson <mjnichol@ualberta.ca>
2014-01-23 23:32:29 -0700

    tossed in some getters and setters

--- src/com/killerkounter/Counter.java
+++ src/com/killerkounter/Counter.java
@@ -9,20 +9,25 @@ public class Counter {
 	private String name;
 	private ArrayList<Date> dates;
 	
+	// add an exception to catch null names and repeated names
 	public Counter(String name) {
 		this.count = 0;
 		this.name = name;
 		dates = new ArrayList<Date>();
 	}
 	
+	// Reset the counter
 	public void Reset(){
 		count = 0;
 		dates = new ArrayList<Date>();
 	}
 	
+	// add an exception if the string is Null
 	public void Rename(String name){
 		this.name = name;
 	}
+	
+	// Increment the counter
 	public void Increment(){
 		count++;
 		dates.add(new Date());
@@ -33,10 +38,12 @@ public class Counter {
 		return count;
 	}
 
+	// Get the counter name
 	public String getName() {
 		return name;
 	}
 
+	// Get the list of dates. Will be used for statistics
 	public ArrayList<Date> getDates() {
 		return dates;
 	}
