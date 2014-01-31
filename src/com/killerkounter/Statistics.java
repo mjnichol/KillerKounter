a4482fe0aa3716b619cb6c5717b4e729e5f1aa20
Mike Nicholson <mjnichol@ualberta.ca>
2014-01-31 14:17:31 -0700

    working on the stats class, not yet done!

--- src/com/killerkounter/Statistics.java
+++ src/com/killerkounter/Statistics.java
@@ -1,13 +1,75 @@
 package com.killerkounter;
 
-// Not sure exactly how to implement this, perhaps bundle the functionality into the counter class?
+import java.util.ArrayList;
+import java.util.Date;
+
+
+// This class will handle all of the statistics functionalities for the application
 public class Statistics {
 
-	/*
-	private Counter counter;
+
+	// Take a counter only! The user will have to select a counter
+	// This will then take them to a new view
 	
+	private Counter counter;
+	// default constructor
 	public Statistics(Counter counter) {
 		this.counter = counter;
 	}
-*/
+	
+	/*
+	The below methods all return an array list of strings which lists
+	the counts for each time unit in the range
+	*/
+	
+	public ArrayList<String> MinutelyCounts(){
+		ArrayList<String> min_counts = new ArrayList<String>();
+		
+		int count = 0;
+		
+		// make sure there are counts for the counter
+		if (counter.getDates() == null)
+			return null;
+		
+		Date temp = counter.getDates().get(0);
+		
+		
+		// iterate over the dates
+		for(Date date : counter.getDates()){
+			
+			// if the hour switches add to the string and refresh count
+			if(temp.getHours() < date.getHours() && !(date.equals(temp))){
+				
+			}
+			
+		}
+		
+		return min_counts;		
+	}
+
+	public ArrayList<String> HourlyCounts(){
+		ArrayList<String> hour_counts = new ArrayList<String>();
+		
+		return hour_counts;		
+	}
+	
+	public ArrayList<String> DailyCounts(){
+		ArrayList<String> day_counts = new ArrayList<String>();
+		
+		return day_counts;		
+	}
+	
+	public ArrayList<String> WeeklyCounts(){
+		ArrayList<String> week_counts = new ArrayList<String>();
+		
+		return week_counts;		
+	}
+	
+	public ArrayList<String> MonthlyCounts(){
+		ArrayList<String> month_counts = new ArrayList<String>();
+		
+		return month_counts;		
+	}
+	
+	
 }
